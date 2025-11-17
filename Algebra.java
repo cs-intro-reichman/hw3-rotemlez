c4adef7f0505280702029062f370e047a5918023
rotemlez <rotem.lezerovich@post.runi.ac.il>
2025-11-17 18:54:54 +0200

    completed assignment

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,-3));   // 2 + 3
+	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
@@ -26,8 +26,17 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		int sum = x1;
+		if(x2>=0){
 			for (int i=0; i<x2; i++){
 			sum++;
+		}
+	}
+		if(x2<0){
+			x2 = 0-x2;
+			for (int i=0; i<x2; i++){
+			sum--;
+		}
+			
 		}
 		return sum;
 	}
@@ -35,18 +44,40 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		int b = x1;
-		for (int i=0; i<=x2; i++){
-			b = x1--;
+		if(x2>=0){
+			for (int i=0; i<x2; i++){
+			b--;
 		}
+	}
+		if(x2<0){
+			x2 = 0-x2;
+			for (int i=0; i<x2; i++){
+			b++;
+		}
+	}
 		return b;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		int t = 0;
-		for (int i=0; i<x2; i++){
-			t = plus(t,x1) ;
+		if (x1==0 || x2==0){
+			return 0;
 		}
+		boolean negativeX =false;
+		if ((x1<0 && x2>0) || (x1>0 && x2<0))
+			negativeX = true;
+		int t=0;
+		if (x1<0)
+			x1= minus(0, x1);
+		if (x2<0){
+			x2 = minus(0, x2);	
+		}	
+		for (int i=0; i<x2; i++){
+			t = plus(t, x1);
+		}
+		if (negativeX)
+		return minus(0, t);
+		
 		return t;
 	}
 
@@ -63,14 +94,24 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int d = 0;
-		if(x2==0)
+		if(x1==0 || x2==0)
 		{
 			return 0;
 		}
+		boolean negativeX =false;
+		if ((x1<0 && x2>0) || (x1>0 && x2<0))
+			negativeX = true;
+		if (x1<0)
+			x1= minus(0, x1);
+		if (x2<0){
+			x2 = minus(0, x2);	
+		}	
 		while (x1>=x2){
 			x1 = minus(x1,x2);	
 			d++;
 		}
+		if (negativeX)
+		return minus(0, d);
 		return d;
 	}
 
@@ -83,9 +124,24 @@ public class Algebra {
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
+		if (x<0){
+			return 0; 
+		}
+		if (x==0 || x==1){
+			return x;
+		}
+		int low = 1;
+		int high = x;
 		int s = 0;
-		while (times(s, s)<x){
-			s++;
+		while (low<=high){
+			int mid = plus(low, div(minus(high,low),2));
+			if (times(mid, mid) <= x){
+				s = mid;
+				low = plus(mid, 1);
+			}
+			else{
+				high = minus(mid, 1);
+			}
 		}
 		return s;
 	}	  	  
